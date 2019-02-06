093add006b93a8455a1d78360693cc6a1c81c09b
강정호 <jeongho.kang@dramancompany.com>
2019-02-06 14:33:52 +0900

    중복로그인시 에러메시지 출력

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,11 @@
 class SessionsController < ApplicationController
-
+  before_action :logged_in_redirect, only: [:new, :create]
 
   def new
-
 
   end
 
   def create
-    #user_id를 세션에 넣어야 한다.
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       # good scenario
@@ -21,7 +19,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    sesison[:user_id] = nil
+    session[:user_id] = nil
     flash[:success] = "You have successfully logged out"
     redirect_to login_path
   end
@@ -29,9 +27,14 @@ class SessionsController < ApplicationController
 
   private
 
-    def  request_params
-      params.require(:session).permit(:username, :password)
+    def logged_in_redirect
+      if logged_in?
+        flash[:error] = "You are already logged in"
+        redirect_to root_path
+      end
     end
+
+
 
 
 end
